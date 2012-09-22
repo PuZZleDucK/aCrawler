23ad3c56a41c2be7e05f4c1ac4b4e7d26a12e61a
puzzleduck <puzzleduck@gmail.com>
2012-09-22 16:02:19 +1000

    first class

---
+++ aCrawl/src/com/puzzleduck/crawler/ACrawler.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.puzzleduck.crawler;
+
+/**
+ * @author bminerds
+ *
+ */
+public class ACrawler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
